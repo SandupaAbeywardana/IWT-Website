67c8d02ffd5059388c774c1dba5eed079c687318
sachi-cyber <sachinthakaa86@gmail.com>
2022-05-22 08:05:53 +0530

    contact css

--- Project/php/login.php
+++ Project/php/login.php
@@ -7,48 +7,29 @@
 
 // Create connection
 
-$errors = [];
+session_start();
+if(isset($_POST['save']))
+{
+    extract($_POST);
 
-      $connection=mysqli_connect("localhost","root","","textile");
-	  
-	  if ($connection==false)  
-	    {
-      die("Connection failed: ". mysqli_connect_error());
-	    }
-        
-       $email=$_POST['email']; 
-       $password=$_POST['password'];
+    include 'config.php';
 
+    $email=$_POST['email']; 
+    $password=$_POST['password'];
 
-if (count($errors) === 0) {
-        $query = "SELECT * FROM new_customers WHERE Email=? LIMIT 1";
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param('ss', $username, $password);
-
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
-            $user = $result->fetch_assoc();
-            if (password_verify($password, $user['password'])) { // if password matches
-                $stmt->close();
-
-                $_SESSION['id'] = $user['id'];
-                $_SESSION['username'] = $user['username'];
-                $_SESSION['email'] = $user['email'];
-                $_SESSION['verified'] = $user['verified'];
-                $_SESSION['message'] = 'You are logged in!';
-                $_SESSION['type'] = 'alert-success';
-                header('location: index.php');
-                exit(0);
-            } else { // if password does not match
-                $errors['login_fail'] = "Wrong username / password";
-            }
-        } else {
-            $_SESSION['message'] = "Database error. Login failed!";
-            $_SESSION['type'] = "alert-danger";
-        }
+    $sql=mysqli_query($conn,"SELECT * FROM registration where email='$email' and Password='$password'");
+    $row  = mysqli_fetch_array($sql);
+    if(is_array($row))
+    {
+        $_SESSION["Email"]=$row['Email'];
+        $_SESSION["First_Name"]=$row['First_Name'];
+        $_SESSION["Last_Name"]=$row['Last_Name']; 
+        header("Location: ../HTML/profile.html"); 
+    }
+    else
+    {
+        echo "Invalid Email ID/Password";
     }
 }
-		mysqli_close($connection);
-
 
 ?>
