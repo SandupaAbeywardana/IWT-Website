893c31f25b6eb7637ffef360ab9cb6385a143ab1
Sandupa Abeywardana <sandupa.isum@gmail.com>
2022-05-22 21:12:30 +0530

    rate us

---
+++ Project/php/comment.php
@@ -0,0 +1,69 @@
+<html>
+
+<head>
+
+    <link rel="stylesheet" type="text/css" href="../CSS/cstyle.css">
+
+    <title>Rate Our Service</title>
+</head>
+
+<body>
+
+    <div class="menu">
+        <ul>
+            <li><img src="../Images/Logo/Logo.jpg" width="50px" height="50px"></li>
+            <li><a href="../php/index.php"><b>Home</b></a></li>
+            <li><a href="achievements.html"><b>Achievements</b></a></li>
+            <li><a href="products.html"><b>Product</b></a></li>
+            <li><a href="contact.html"><b>Contact us</b></a></li>
+        </ul>
+    </div>
+
+
+
+
+    <div class="container">
+
+        <form method="POST" action="insert.php">
+
+            <div class="top">
+                <h2>Rate Our Service</h2>
+            </div>
+
+            <div class="comm">
+                <textarea rows=20 cols=50 placeholder="Enter Your Comment.." name="description">
+</textarea>
+            </div>
+            <br>
+            <div class="sub">
+                <input type="submit" name="submit" value="Submit">
+            </div>
+
+        </form>
+
+    </div>
+
+    <section class="footer">
+        <div class="social">
+            <a href="https://www.instagram.com"><img src="../Images/insta.jpeg" class="insta" width="50px"
+                    height="50px"></a>
+            <a href="https://www.facebook.com"><img src="../Images/fb.jpeg" class="fb" width="50px" height="50px"></a>
+            <a href="https://www.snapchat.com"><img src="../Images/snap.jpeg" class="snap" width="50px" height="50px"></a>
+            <a href="https://www.twitter.com"><img src="../Images/twitter.jpeg" class="twitter" width="50px"
+                    height="50px"></a>
+        </div>
+        <ul class="list">
+            <li><a href="index.php">Home</a></li>
+                <li><a href="../HTML/about.html">About</a></li>
+                <li><a href="../HTML/terms.html">Terms</a></li>
+                <li><a href="../HTML/privacy.html">Privacy & Policy</a></li>
+        </ul>
+        <p class="copyright">
+            The Chosen One Textile & Garment Management System @ 2022
+        </p>
+    </section>
+
+
+</body>
+
+</html>
